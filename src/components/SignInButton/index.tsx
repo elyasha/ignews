82c07a62d3f2c197d5a73b19c37773230eb9b171
Matheus Elyasha <matheuselyasha@gmail.com>
2021-07-04 18:21:17 -0300

    componente sign in button

---
+++ src/components/SignInButton/index.tsx
@@ -0,0 +1,19 @@
+import { FaGithub } from 'react-icons/fa';
+import {FiX} from 'react-icons/fi'
+import styles from './styles.module.scss';
+
+export function SignInButton() {
+  const isUserLoggedIn = true
+  return isUserLoggedIn ? (
+    <button className={styles.signInButton} type="button">
+      <FaGithub color="#04d361" />
+      Matheus Elyasha
+      <FiX color="#737380" className={styles.closeIcon} />
+    </button>
+  ) : (
+    <button className={styles.signInButton} type="button">
+      <FaGithub color="#eba417" />
+      Sign In with GitHub
+    </button>
+  );
+}
