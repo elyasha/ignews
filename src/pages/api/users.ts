41d81908e9b3fe38923e3e36c54965796564faeb
Matheus Elyasha <matheuselyasha@gmail.com>
2021-07-19 11:04:48 -0300

    api routes no next.js

---
+++ src/pages/api/users.ts
@@ -0,0 +1,13 @@
+import {NextApiRequest, NextApiResponse} from 'next'
+
+export default (request : NextApiRequest, response : NextApiResponse) => {
+	const users = [
+		{ id: 1, name: 'Matheus Elyasha'},
+		{ id: 2, name: 'Dani'},
+		{ id: 3, name: 'Rafa'},
+		{ id: 4, name: 'Eduardo'},
+	]
+
+    return response.json(users)
+
+}
