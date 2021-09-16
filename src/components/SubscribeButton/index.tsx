814b679f71f455a86f974d73a15cccb74657bd3e
Matheus Elyasha <matheuselyasha@gmail.com>
2021-09-15 23:43:13 -0300

    gerando sessão de checkout

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,14 +1,37 @@
+import { signIn, useSession } from 'next-auth/client';
+import { stripe } from '../../services/stripe';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
   priceId: string
 }
 
+// Posso executar código backend
+// getServerSideProps (SSR)
+// getStaticProps (SSG)
+// API routes
 
-export function SubscribeButton({priceId} : SubscribeButtonProps) {
-	return (
-    <button type="button" className={styles.subscribeButton}>
+export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const [session] = useSession()
+
+  function handleSubscribe() {
+    if (!session) {
+      signIn('github')
+      return;
+    }
+
+    // Criação da checkout session
+    // strip checkout session
+    
+
+
+  }
+
+  return (
+    <button
+      type="button"
+      className={styles.subscribeButton}>
       Subscribe now
     </button>
-	)
+  )
 }
