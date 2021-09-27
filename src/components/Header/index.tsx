1a2c09cc85d1ee9ba74fdfada342f4be4494c84a
Matheus Elyasha <matheuselyasha@gmail.com>
2021-09-26 21:43:19 -0300

    componente activelink e cloneElement

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,21 +1,26 @@
 import { SignInButton } from '../SignInButton';
-import Link from 'next/link';
 import styles from './styles.module.scss'
+
+import { ActiveLink } from '../ActiveLink';
+
+
 export function Header() {
-	return (
+
+
+  return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
-        <img src='/images/logo.svg' alt='ig.news'/>
+        <img src='/images/logo.svg' alt='ig.news' />
         <nav>
-          <Link href="/">
-          <a className={styles.active}>Home</a>
-          </Link>
-          <Link href="/posts" prefetch>
-          <a>Posts</a>
-          </Link>
+          <ActiveLink activeClassName={styles.active} href={'/'}>
+            <a className={styles.active}>Home</a>
+          </ActiveLink>
+          <ActiveLink prefetch activeClassName={styles.active} href={'/posts'}>
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
         <SignInButton />
       </div>
     </header>
-	);
+  );
 }
