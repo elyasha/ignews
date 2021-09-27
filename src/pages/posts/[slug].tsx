29514258fab5cf9fe73c87585a36a30168192f0b
Matheus Elyasha <matheuselyasha@gmail.com>
2021-09-26 22:29:12 -0300

    validando assinatura ativa

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -43,29 +43,37 @@ export const getServerSideProps: GetServerSideProps = async ({ req, params }) =>
 
 	const { slug } = params;
 
-	// if (!session) {
+	// console.log(session)
 
-	// }
+	if (!session.activeSubscription) {
+		return {
+			redirect: {
+				destination: '/',
+				permanent: false,
+			}
 
-	const prismic = getPrismicClient(req)
-
-	const response = await prismic.getByUID('publication', String(slug), {})
-
-	const post = {
-		slug,
-		title: RichText.asText(response.data.title),
-		content: RichText.asHtml(response.data.content),
-		updatedAt: new Date(response.last_publication_date).toLocaleDateString('pt-BR', {
-			day: '2-digit',
-			month: 'long',
-			year: 'numeric'
-		})
+		}
 	}
 
+		const prismic = getPrismicClient(req)
+
+		const response = await prismic.getByUID('publication', String(slug), {})
+
+		const post = {
+			slug,
+			title: RichText.asText(response.data.title),
+			content: RichText.asHtml(response.data.content),
+			updatedAt: new Date(response.last_publication_date).toLocaleDateString('pt-BR', {
+				day: '2-digit',
+				month: 'long',
+				year: 'numeric'
+			})
+		}
 
 
-	return {
-		props: { post }
+
+		return {
+			props: { post }
+		}
+
 	}
-
-}
